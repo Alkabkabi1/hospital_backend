aade9b53bdc0aa6fd88208820fbe7e2312a361bc
Joory Halabi <Joory6067@hotmail.com>
2025-08-04 14:51:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use('/api/evaluation', evaluationRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api", require("./routes/checkSession")); // هذا مهم
 app.use("/api", publicRoutes);
-app.use("/api", policyRoutes);
+app.use("/api/admin", require("./routes/policies"));
 
 
 // ✅ عرض ملفات الواجهة
